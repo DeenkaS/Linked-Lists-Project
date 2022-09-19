08ebabf64bc2cbb4e05e1c6b06c2616b4b0037fb
Deniel Saskin <96053994+DeenkaS@users.noreply.github.com>
2022-09-19 14:02:00 +0200

    Start of linked list project

---
+++ LinkedLists.java
@@ -0,0 +1,27 @@
+public class LinkedLists {
+    int head;
+    LinkedList tail;
+
+    public LinkedList(int item, LinkedList list) {
+        head = item;
+        tail = list;
+    }
+
+    public int head() {
+        return this.head;
+    }
+    
+    public Linkedlist tail() {
+        return this.tail;
+    }
+
+    public void append(LinkedList b) {
+        LinkedList nxt = this;
+        while (nxt.tail != null) {
+        nxt = nxt.tail;
+        }
+        nxt.tail = b;
+        }
+        
+
+}
