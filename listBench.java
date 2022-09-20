508d7f055b81d05f1715334dce25162135f6192c
DeenkaS <96053994+DeenkaS@users.noreply.github.com>
2022-09-20 12:29:17 +0200

    almost finished

--- listBench.java
+++ listBench.java
@@ -1,38 +1,67 @@
 import java.util.Random;
-import java.util.random.*;
+
 public class listBench {
-    public static void main(String []args){
+    public static void main(String[] args) {
         int k = 100_000;
-        int[] bencharray = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600};
+        int contstantListLength = 99;
+         int[] bencharray = { 99, 199, 299, 399, 499, 599, 699, 799, 899, 999, 1099, 1199, 1299, 1399, 1499, 1599};
         Random rnd = new Random();
-        LinkedList benchList = new LinkedList(1);
+        LinkedList variableList = new LinkedList(1);
+        LinkedList constantList = new LinkedList(1);
         long t0 = 0;
         long tsum = 0;
 
-        for(int i = 0; i< 10 ;i++){
-            
-        }
-
-
-        System.out.println("Runtimes for appending a linked list n times: ");
+        System.out.println("Runtimes for appending a variable length linked list to the end of a constant length linked list: ");
 
         for (int n : bencharray) {
-            int[] array1 = new int[n];
 
             for (int i = 0; i < k; i++) {
 
-                for (int j = 0; j < n; j++) {
-                    array1[i] = rnd.nextInt(100);
+                for (int l = 0; l < contstantListLength; l++) {
+                    constantList.append(new LinkedList(rnd.nextInt(n)));
                 }
 
-                for(int j = 0; j < n; j++){
-                    
+                for (int l = 0; l < n; l++) {
+                    variableList.append(new LinkedList(rnd.nextInt(n)));
                 }
 
-                
+                t0 = System.nanoTime();
+                constantList.append(variableList);
+                tsum += System.nanoTime() - t0;
+                constantList = new LinkedList(1);
+                variableList = new LinkedList(1);
+
             }
 
-            System.out.print("(" + n + "," + (tsum / k)+ ")");
+            System.out.print("(" + (n+1) + "," + (tsum / k) + ")");
+
+        }
+
+        System.out.println("\n\n");
+
+        System.out.println("Runtimes for appending a constant length linked list to the end of a variable length linked list: ");
+
+        for (int n : bencharray) {
+
+            for (int i = 0; i < k; i++) {
+
+                for (int l = 0; l < contstantListLength; l++) {
+                    constantList.append(new LinkedList(rnd.nextInt(n)));
+                }
+
+                for (int l = 0; l < n; l++) {
+                    variableList.append(new LinkedList(rnd.nextInt(n)));
+                }
+
+                t0 = System.nanoTime();
+                variableList.append(constantList);
+                tsum += System.nanoTime() - t0;
+                constantList = new LinkedList(1);
+                variableList = new LinkedList(1);
+
+            }
+
+            System.out.print("(" + (n+1) + "," + (tsum / k) + ")");
 
         }
 
